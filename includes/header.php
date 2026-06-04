cdfaa68a0b172095002f1c8053b4aac221319e4e
FlorianvdM <344612@student.mboutrecht.nl>
2026-06-04 16:14:43 +0200

    style aangepast

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?= htmlspecialchars($paginaTitel ?? 'TheaterAurora') ?></title>
-  <link rel="stylesheet" href="assets/css/style.css" /> <!-- LET OP WELK STYLE BESTAND IS GELINKED -->
+  <link rel="stylesheet" href="assets/css/meldingen.css" /> <!-- LET OP WELK STYLE BESTAND IS GELINKED -->
 </head>
 <body>
 
