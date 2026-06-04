9f1511a5068c736c95033596dc3b7fcef090a1ef
FlorianvdM <344612@student.mboutrecht.nl>
2026-06-04 16:05:08 +0200

    Nav aangepast

--- includes/header.php
+++ includes/header.php
@@ -15,6 +15,14 @@
 <body>
 
   <header class="site-header">
-    <div class="header-logo">Logo</div>
-    <nav class="header-nav">Nav Links</nav>
+    <div class="header-logo"><a href="index.php" style="color: inherit; text-decoration: none;">TheaterAurora</a></div>
+    <nav class="header-nav">
+      <ul style="display: flex; gap: 15px; list-style: none; margin: 0; padding: 0;">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="voorstellingen.php">Voorstellingen</a></li>
+        <li><a href="tickets.php">Tickets</a></li>
+        <li><a href="meldingen.php">Meldingen</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </nav>
   </header>
