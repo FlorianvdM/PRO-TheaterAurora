829636effafa6fc892a86908346f5a4103be2d63
Cain Brouwer <caindavid2008@gmail.com>
2026-07-09 02:32:35 +0200

    11. Wijzig annuleer knop naar 'Terug'

--- account-wijzig.php
+++ account-wijzig.php
@@ -136,7 +136,7 @@ require_once __DIR__ . '/includes/header.php';
                 </select>
             </div>
             <button type="submit" class="knop knop-primair">Wijzigingen opslaan</button>
-            <a href="accounts.php" class="knop knop-secundair">Annuleren</a>
+            <a href="accounts.php" class="knop knop-secundair">Terug</a>
         </form>
         <?php endif; ?>
     </div>
