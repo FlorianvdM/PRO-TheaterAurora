bb45e35864737686af1f3ee843a3fcbceba16931
Cain Brouwer <caindavid2008@gmail.com>
2026-07-09 02:31:16 +0200

    2. Wijzig paginatitel van 'Account wijzigen' naar 'Wijzig account'

--- account-wijzig.php
+++ account-wijzig.php
@@ -5,7 +5,7 @@ session_start();
 require_once __DIR__ . '/includes/toegang.php';
 vereistToegang(['Admin']);
 
-$paginaTitel = 'Account wijzigen';
+$paginaTitel = 'Wijzig account';
 $error = null;
 $succes = false;
 
