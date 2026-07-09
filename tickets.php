e93f7deb1e5455b89c550d7b78c07da87f883681
FlorianvdM <344612@student.mboutrecht.nl>
2026-07-09 11:21:37 +0200

    resterende push

--- tickets.php
+++ tickets.php
@@ -9,7 +9,6 @@ $paginaTitel = 'Tickets – TheaterAurora';
 
 $kanTicketsBeheren = isset($_SESSION['rol']) && in_array($_SESSION['rol'], ['Admin', 'Medewerker']);
 
-
 $tickets = [];
 if ($pdo !== null) {
     $sql = 'SELECT t.Id, t.Nummer, t.Barcode, t.Datum, t.Tijd, t.Status, t.Isactief,
@@ -127,11 +126,7 @@ require_once 'includes/header.php';
           <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"></path></svg>
           <h3 class="tickets-lege-titel">Geen tickets gevonden</h3>
           <p class="tickets-lege-tekst">Er zijn nog geen tickets geregistreerd.</p>
-<<<<<<< HEAD
           <?php if ($kanTicketsBeheren): ?>
-=======
-          <?php if (isset($_SESSION['rol']) && in_array($_SESSION['rol'], ['Admin', 'Medewerker'])): ?>
->>>>>>> feature-bestaande-melding-versturen
             <a href="ticket-toevoegen.php" class="knop knop-primair">Nieuw ticket toevoegen</a>
           <?php endif; ?>
         </div>
